8783eed204516c32855373ae6c33101a5903cf52
Gustavo <gustavobiggg64@gmail.com>
2025-10-05 00:33:44 -0300

    feat: create new fuction in javascript, auto complete cep

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
-let input_CEP = document.getElementById("input_cep")
+document.addEventListener("DOMContentLoaded", () => {
+    const inputCEP = document.getElementById("input_CEP");
+    const inputRua = document.getElementById("input_RUA");
+    const inputEstado = document.getElementById("input_ESTADO");
+    const inputBairro = document.getElementById("input_BAIRRO");
 
-input_CEP.addEventListener(() =>)
+    // Quando sair do campo CEP
+    inputCEP.addEventListener("blur", () => {
+        let cep = inputCEP.value.replace(/\D/g, ''); // só números
+
+        if (cep.length === 8) {
+            fetch(`https://viacep.com.br/ws/01001000/json/`)
+                .then(response => response.json())
+                .then(data => {
+                    if (!data.erro) {
+                        inputRua.value = data.logradouro;
+                        inputBairro.value = data.bairro;
+                        inputEstado.value = data.uf;
+                    } else {
+                        alert("CEP não encontrado!");
+                        inputRua.value = "";
+                        inputBairro.value = "";
+                        inputEstado.value = "";
+                    }
+                });     
+    });
+});
